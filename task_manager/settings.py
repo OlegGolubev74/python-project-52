25736627d0657a9da11628ac7d2dd30aceab17f3
oleg <oleg.golubev74@gmail.com>
2026-01-18 00:10:12 +0300

    step 2.2

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -134,3 +134,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"# шаг 2
+
+# шаг 2
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
